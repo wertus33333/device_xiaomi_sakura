1c18db8a2740d0e4309688dcac2e9297f605ba50
Jyotiraditya <jyotiraditya182@gmail.com>
2018-11-19 12:37:05 +0530

    sakura: Initial Trees
    Signed-off-by: Jyotiraditya <jyotiraditya182@gmail.com>

---
+++ lineage.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/xiaomi/sakura/full_sakura.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_sakura
+BOARD_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sakura-user 8.1.0 OPM1.171019.019 V9.6.4.0.ODMMIFD release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "xiaomi/sakura/sakura:8.1.0/OPM1.171019.019/V9.6.4.0.ODMMIFD:user/release-keys"
